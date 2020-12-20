04adf8d6997b505e925c2434d8bc8e59fca41a51
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2020-12-21 01:08:31 +0530

    Update journy_title.dart

--- lib/components/journy_title.dart
+++ lib/components/journy_title.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 
 class Journy extends StatelessWidget {
+  final Color bgColor;
+  final Color txtColor;
+  final String text;
+  final Function fn;
+  Journy({this.bgColor, this.txtColor, this.text,this.fn});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Color(0xffFFFF00),
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
+    return GestureDetector(
+      onTap: fn,
+      child: Container(
+        //padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
         ),
-      ),
-      width: 150,
-      //color: Color(0xffFFFF00),
-      //height: 50,
-      child: Center(
-        child: Text(
-          'Journy',
-          style: TextStyle(
-            color: Color(0xffFF0000),
-            fontFamily: 'Pacifico',
-            fontSize: 25,
+        width: 150,
+        //color: Color(0xffFFFF00),
+        //height: 50,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: txtColor,
+              fontFamily: 'Pacifico',
+              fontSize: 25,
+            ),
           ),
         ),
       ),
