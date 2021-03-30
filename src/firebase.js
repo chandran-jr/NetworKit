acde5bcc5f735876209d98ef4fbf2b730e1d6fee
Govind B Chandran <govindchandran235@gmail.com>
2021-03-30 17:00:59 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9CWbwlT7h_lMTe0b2XCjbYU9HGlNHgsk",
+    authDomain: "networkit-62da2.firebaseapp.com",
+    projectId: "networkit-62da2",
+    storageBucket: "networkit-62da2.appspot.com",
+    messagingSenderId: "651810872816",
+    appId: "1:651810872816:web:252cefb4bd6d9071379135"
+  };
