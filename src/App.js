30a42259aa1c9f0bf820b6919e6d6d83124666e8
Govind B Chandran <govindchandran235@gmail.com>
2021-04-04 14:43:54 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
 
     <Header/>
+
+    <div className="app__body">
+        <Sidebar/>
+    </div>
+
       
     </div>
   );
