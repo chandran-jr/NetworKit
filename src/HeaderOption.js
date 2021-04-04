d2170a2f0b86e542689ae7ef76d0efe5458d5181
Govind B Chandran <govindchandran235@gmail.com>
2021-04-04 14:33:06 +0530

    Add files via upload

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './HeaderOption.css';
+import {Avatar} from '@material-ui/core';
 
-function HeaderOption({Icon, title}) {
+function HeaderOption({avatar, Icon, title}) {
     return (
         <div className="headerOption">
             {Icon && <Icon className="headerOption__icon"/>}
+            {avatar && <Avatar className="headerOption__icon" src={avatar} alt="profilepic" /> }
             <h3 className="headerOption__title">{title}</h3>
         </div>
     )
