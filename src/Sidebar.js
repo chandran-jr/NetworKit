b5484eeee4b16468e658af3b78434a8954cd2653
Govind B Chandran <govindchandran235@gmail.com>
2021-04-04 20:53:29 +0530

    Add files via upload

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,15 @@ import './Sidebar.css';
 function Sidebar() {
     return (
         <div className="sidebar">
+
+        <div className="sidebar__top">
+
+        </div>
+
+        
+        <div className="sidebar__bottom">
+            
+        </div>
             
         </div>
     )
