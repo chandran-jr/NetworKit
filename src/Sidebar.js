7d468c62dd832132f59e21961d12c8ebc14317a5
Govind B Chandran <govindchandran235@gmail.com>
2021-04-05 22:28:39 +0530

    Add files via upload

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,6 +10,7 @@ function Sidebar() {
             <img src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y29tcHV0ZXJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="bg"/>
             <Avatar className="sidebar__avatar" src="" alt="me" />
             <h2>Govind B Chandran</h2>
+            <h4>govindchandran150@gmail.com</h4>
             <h4>React Developer</h4>
         </div>
 
