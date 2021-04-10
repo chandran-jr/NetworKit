fb6ff1e4407adeff27387fda0d2b6360b38a69fd
Govind B Chandran <govindchandran235@gmail.com>
2021-04-10 16:50:54 +0530

    Add files via upload

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,7 @@
 import CreateIcon from '@material-ui/icons/Create';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import ImageIcon from '@material-ui/icons/Image';
 import React from 'react';
 import './Feed.css';
@@ -19,6 +22,9 @@ function Feed() {
 
                     <div className="feed__inputOptions">
                         <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+                        <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+                        <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+                        <InputOption Icon={CalendarViewDayIcon} title="Write Article" color="#7FC15E" />
 
                     </div>
 
