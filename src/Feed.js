d09cee5b202ec31aef3b040aaa13e3c27832d701
Govind B Chandran <govindchandran235@gmail.com>
2021-04-10 14:59:13 +0530

    Add files via upload

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,16 @@
+import CreateIcon from '@material-ui/icons/Create';
 import React from 'react';
 import './Feed.css';
 
 function Feed() {
     return (
-        <div>
+        <div className="feed">
+
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                </div>
+            </div>
             
         </div>
     )
