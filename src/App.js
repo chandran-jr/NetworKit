3288fba880b17eb5d4ed9e119daffd603f58da3e
Govind B Chandran <govindchandran235@gmail.com>
2021-04-10 14:53:42 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
     <div className="app__body">
         <Sidebar/>
+        <Feed />
     </div>
 
       
---
+++ src/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Feed.css';
+
+function Feed() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Feed
