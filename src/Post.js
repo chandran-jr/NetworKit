9e3a57033e50ae3aa3c5a12a3e72767ba3dbb467
Govind B Chandran <govindchandran235@gmail.com>
2021-04-11 01:20:05 +0530

    Add files via upload

---
+++ src/Post.js
@@ -0,0 +1,24 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './Post.css';
+
+function Post({name, description, message, photoUrl}) {
+    return (
+        <div className="post">
+            <div className="post__header">
+                <Avatar/>
+                <div className="post__info">
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+
+                <div className="post__body">
+                    <p>{message}</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Post
