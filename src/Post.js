802b6801c82e842ab333c67927a6be10f2d0ecac
Govind B Chandran <govindchandran235@gmail.com>
2021-04-11 10:48:52 +0530

    Add files via upload

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,10 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import InputOption from './InputOption';
 import './Post.css';
 
 function Post({name, description, message, photoUrl}) {
@@ -12,10 +17,18 @@ function Post({name, description, message, photoUrl}) {
                     <p>{description}</p>
                 </div>
             </div>
-            
+
             <div className="post__body">
                     <p>{message}</p>
                 </div>
+
+            <div className="post__buttons">
+                <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+                <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+                <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+                <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
+            </div>
+
         </div>
     )
 }
