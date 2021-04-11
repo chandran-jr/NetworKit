510575a1252f65dff84bdbb0a48503771a489dbd
Govind B Chandran <govindchandran235@gmail.com>
2021-04-11 14:09:34 +0530

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB9CWbwlT7h_lMTe0b2XCjbYU9HGlNHgsk",
     authDomain: "networkit-62da2.firebaseapp.com",
@@ -6,3 +8,5 @@ const firebaseConfig = {
     messagingSenderId: "651810872816",
     appId: "1:651810872816:web:252cefb4bd6d9071379135"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
