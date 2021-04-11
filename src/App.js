636cfa7f91b5528026792dfcd402cc873bee3618
Govind B Chandran <govindchandran235@gmail.com>
2021-04-11 21:45:05 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import {useSelector} from 'react-redux';
+import {selectUser} from './features/counter/userSlice';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Login from './Login';
 
 function App() {
+ 
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
 
     <Header/>
 
-    <div className="app__body">
+    {!user ? (<Login/>): (
+      <div className="app__body">
         <Sidebar/>
         <Feed />
     </div>
+    ) }
 
       
     </div>
--- src/features/counter/userSlice.js
+++ src/features/counter/userSlice.js
@@ -6,27 +6,17 @@ export const userSlice = createSlice({
     user: null,
   },
   reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    login: (state, action) => {
+      state.value = action.payload;
     },
-    decrement: state => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    logout: (state) => {
+      state.user = null;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectUser = state => state.user.user;
 
 export default userSlice.reducer;
