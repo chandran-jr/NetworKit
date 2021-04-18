81d701c8cb6ae64e7caa521b9a71940615f7a02c
Govind B Chandran <govindchandran235@gmail.com>
2021-04-18 11:53:33 +0530

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -37,6 +37,16 @@ function Login() {
 
     const loginToApp = (e) => {
         e.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password,)
+        .then((userAuth) => {
+            dispatch(login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: userAuth.user.displayName,
+                photoUrl: userAuth.user.photoURL,
+            }));
+        }).catch((error) => alert(error));
     };
 
     return (
