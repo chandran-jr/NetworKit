3e1d677816f8ecdacce129ac936546a46ad592c7
Govind B Chandran <govindchandran235@gmail.com>
2021-04-18 08:33:41 +0530

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -32,7 +32,7 @@ function Login() {
                     photoUrl: profilePicture,
                 }));
             });
-        });
+        }).catch((error) => {alert(error.message);});
     };
 
     const loginToApp = (e) => {
