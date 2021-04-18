e897e97142d3e391b74e0c2848b47bcaaa8e7732
Govind B Chandran <govindchandran235@gmail.com>
2021-04-18 21:52:32 +0530

    Add files via upload

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './HeaderOption.css';
 import {Avatar} from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/counter/userSlice';
 
 function HeaderOption({avatar, Icon, title, onClick}) {
+
+    const user = useSelector(selectUser);
+
     return (
         <div onClick={onClick} className="headerOption">
             {Icon && <Icon className="headerOption__icon"/>}
-            {avatar && <Avatar className="headerOption__icon" src={avatar} alt="profilepic" /> }
+            {avatar && (<Avatar className="headerOption__icon" alt="profilepic">{user.email[0]}</Avatar>)}
             <h3 className="headerOption__title">{title}</h3>
         </div>
     )
