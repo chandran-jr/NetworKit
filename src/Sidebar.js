b62be0f526fb750694752daa1fe04b284d78aa55
Govind B Chandran <govindchandran235@gmail.com>
2021-04-18 21:35:33 +0530

    Add files via upload

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -21,7 +21,7 @@ function Sidebar() {
 
         <div className="sidebar__top">
             <img src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y29tcHV0ZXJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="bg"/>
-            <Avatar className="sidebar__avatar" src="" alt="me" />
+            <Avatar className="sidebar__avatar" src={user.photoUrl} alt="me" />
             <h2>{user.displayName}</h2>
             <h4 className="sidebar__statemail">{user.email}</h4>
         </div>
