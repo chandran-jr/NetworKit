eb6e7ebb412988a1313bee64581cadb0e410df46
Govind B Chandran <govindchandran235@gmail.com>
2021-04-22 21:56:39 +0530

    Add files via upload

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Widgets.css';
+import InfoIcon from "@material-ui/icons/Info";
 
 function Widgets() {
     return (
         <div className="widgets">
-            
+            <div className="widgets__header">
+                <h2>NetworKit News</h2>
+                <InfoIcon />
+            </div>
         </div>
     )
 }
