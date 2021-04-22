6bd0f3c454638111471805cc88ba324196ebaed4
Govind B Chandran <govindchandran235@gmail.com>
2021-04-22 22:05:47 +0530

    Add files via upload

--- src/Widgets.js
+++ src/Widgets.js
@@ -3,6 +3,22 @@ import './Widgets.css';
 import InfoIcon from "@material-ui/icons/Info";
 
 function Widgets() {
+
+    const newsArticle = (heading,subtitle) => (
+        <div className="widgets__article">
+            <div className="widgets__left">
+
+            </div>
+
+            <div className="widgets__right">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    )
+
+    
+
     return (
         <div className="widgets">
             <div className="widgets__header">
