ace73bc496c80fbbcbdc7ed89c47882d3b54e640
Govind B Chandran <govindchandran235@gmail.com>
2021-04-22 22:08:21 +0530

    Add files via upload

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Widgets.css';
 import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 function Widgets() {
 
     const newsArticle = (heading,subtitle) => (
         <div className="widgets__article">
             <div className="widgets__left">
-
+                <FiberManualRecordIcon/>
             </div>
 
             <div className="widgets__right">
