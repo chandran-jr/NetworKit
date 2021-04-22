313d199ce8e865d9e2e932269c5b627ba2a40dfe
Govind B Chandran <govindchandran235@gmail.com>
2021-04-22 22:31:10 +0530

    Add files via upload

--- src/Widgets.js
+++ src/Widgets.js
@@ -28,7 +28,7 @@ function Widgets() {
                 <InfoIcon />
             </div>
             {newsArticle("Chandran launches NetworKit", "The web app has no potential whatsover😪")}
-            {newsArticle("Covid-19 increases", "Indian condition severe as cases rise rapidly☠")}
+            {newsArticle("Covid-19 increases", "Indian condition severe as cases rise rapidly💀")}
             {newsArticle("Doge hits ATH", "Elon tweet boosts the coin yet again😳")}
             {newsArticle("Neuralink making progress", "Monkey learns to play video games using neurachip😎")}
         </div>
